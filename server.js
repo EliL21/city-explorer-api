1fe3a46aa9bbcb11c977c82f283797c3b259af24
eli bailey <baileyeli19@gmail.com>
2023-01-20 05:22:17 +0000

    updated still needs work

--- server.js
+++ server.js
@@ -1,54 +1,79 @@
 'use strict';
 // **** REQUIRES *****
-console.log('good job!')
+'use strict';
+
+
+// **** REQUIRES ****
 const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
-// const axios = require('axios');
+
+// *** FOR LAB DON'T FORGET TO REQUIRE YOUR STARTER JSON FILE ***
+let data = require('./data/weather.json');
 
 
-
-// *** Create Our Server *****
+// **** Once express is in we need to use it - per express docs
+// *** app === server
 const app = express();
 
+// **** MIDDLEWARE ****
+// *** cors is middleware - security guard that allows us to share resources across the internet **
 app.use(cors());
 
 
+// *** DEFINE A PORT FOR MY SERVER TO RUN ON ***
 const PORT = process.env.PORT || 3002;
 
 
+// **** ENDPOINTS ****
 
-// ***** ENDPOINTS *****
-app.get('/weather', (request, response, next)=>{
-response.status(200).send('Welcome to my server');
+// *** Base endpoint - proof of life
+// ** 1st arg - endpoint in quotes
+// ** 2nd arg - callback which will execute when someone hits that point
+
+// *** Callback function - 2 parameters: request, response (req,res)
+
+
+app.get('/', (request, response) => {
+  response.status(200).send('Welcome to my server');
 });
 
-app.get('/hello', (request, response)=>{
-  let 
-})
 
-// **** CATCHALL ENDPOINT - NEEDS TO BE YOUR LAST DEFINED ENDPOINT ****
+app.get('/hello', (request, response) => {
+  console.log(request.query);
+
+  let firstName = request.query.firstName;
+  let lastName = request.query.lastName;
+
+  response.status(200).send(`Hello ${firstName} ${lastName}! Welcome to my server!`);
+});
+
+
+app.get('/weather', (req,res,next)=>{
+  let lat = req.query.lat;
+  let lon = req.query.lon;
+  let searchQuery = req.query.searchQuery;
+});
+
+
+
+
+// **** CLASS TO GROOM BULKY DATA ****
+
+
+
+// **** CATCH ALL ENDPOINT - NEEDS TO BE YOUR LAST DEFINED ENDPOINT ****
 app.get('*', (request, response) => {
   response.status(404).send('This page does not exist');
 });
 
 
-// ********ERROR HANDLING *****
-app.use((error, request, response next) => {
+// **** ERROR HANDLING - PLUG AND PLAY CODE FROM EXPRESS DOCS ****
+app.use((error, request, response, next) => {
   response.status(500).send(error.message);
 });
 
-// ***** BUILD AN ENDPOINT AN HIT AN API- USE A CLASS TO GROOM THE BULKY DATA
-// app.get('/', (request, respomse, next) =>{
-//   try{
-
-//   } catch{
-    
-//   }
-// })
 
 
-app.listen(PORT, ()=>console.log(`We are running on port: ${PORT}`));
-
-// ****** CATCHALL *****
-// app.get('*', (request, response))
+// ***** SERVER START ******
+app.listen(PORT, () => console.log(`We are running on port: ${PORT}`));
