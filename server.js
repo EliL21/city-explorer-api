e76c04fe2284029e8b73e2bff55e4da5e1c8c87a
eli bailey <baileyeli19@gmail.com>
2023-01-23 04:28:31 +0000

    lab 09

--- server.js
+++ server.js
@@ -1,7 +1,4 @@
 'use strict';
-// **** REQUIRES *****
-'use strict';
-
 
 // **** REQUIRES ****
 const express = require('express');
@@ -9,7 +6,7 @@ require('dotenv').config();
 const cors = require('cors');
 
 // *** FOR LAB DON'T FORGET TO REQUIRE YOUR STARTER JSON FILE ***
-let data = require('./data/weather.json');
+let data = require('./weather.json');
 
 
 // **** Once express is in we need to use it - per express docs
@@ -38,28 +35,81 @@ app.get('/', (request, response) => {
   response.status(200).send('Welcome to my server');
 });
 
+// ****** DEFINE WEATHER ENDPOINT WITH THE FOLLOWING QUERIES - lat, lon, searchQuery
 
-app.get('/hello', (request, response) => {
-  console.log(request.query);
+app.get('/weather', (req, res, next) => {
+  try {
+    // let lat = req.query.lat;
+    // let lon = req.query.lon;
+    let cityName = req.query.searchQuery;
 
-  let firstName = request.query.firstName;
-  let lastName = request.query.lastName;
+    let city = data.find(city => city.city_name.toLowerCase() === cityName.toLocaleLowerCase());
 
-  response.status(200).send(`Hello ${firstName} ${lastName}! Welcome to my server!`);
+    let weatherData = city.data.map(dayobj => new FORECAST(dayobj));
+
+
+
+    res.status(200).send(city);
+
+  } catch (error) {
+    next(error)
+  }
 });
 
+app.get('/movie', async (request, response, next)=>{
+  try {
+    //ToDO- accept search queries - lat,lon, searchQuery - request.query / weather?lat=value&lon=value&searchQuery=value
+    let cityName = request.query.searchQuery;
+    let url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIES_API_KEY}&language=en-US&query=${cityName}&page=1&include_adult=false`
+    console.log(url);
+    let movieBit = await axios.get(url)
+    console.log(movieBit.data.results);
+ 
+    
+    //TODO find the ity in the json data that matches CityName
+   
+    //TODO use a class to minify the bulky data //! change this
+    let movieData = movieBit.data.results.map(movieObj => new Movies(movieObj));
+    console.log(movieData);
+    response.status(200).send(movieData);
 
-app.get('/weather', (req,res,next)=>{
-  let lat = req.query.lat;
-  let lon = req.query.lon;
-  let searchQuery = req.query.searchQuery;
-});
+  } catch (error){
+    // next (error);
+    console.log('this is the error',error)
+  }
+})
+
+// *** Class to groom bulky data ****
+
+class Forecast {
+  constructor(dayObj){
+    this.date = vaild_date;
+    this.description = dayObj.weather.description;
+  }
+}
+
+class Movies {
+  constructor(movieObj){
+    this.title = movieObj.title;
+    this.overview= movieObj.overview;
+    this.poster_path=movieObj.poster_path;
+  }
+}
 
 
 
 
-// **** CLASS TO GROOM BULKY DATA ****
 
+
+
+
+// **** Forecast CLASS TO GROOM BULKY DATA ****
+class FORECAST{
+  constructor(dayobj){
+    this.date =
+    this.description =
+  }
+}
 
 
 // **** CATCH ALL ENDPOINT - NEEDS TO BE YOUR LAST DEFINED ENDPOINT ****
